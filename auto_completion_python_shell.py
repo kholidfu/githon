6b76144247094d94ecc7464c465bd088fca54637
sopier <sopier@gmail.com>
2012-08-06 00:09:31 +0700

    python shell auto-completion

---
+++ auto_completion_python_shell.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+"""
+This is just a note
+
+To make python standard shell get auto-completion feature (just like IPython did):
+
+    import rlcompleter
+    import readline
+    readline.parse_and_bind('tab: complete')
+
+"""
