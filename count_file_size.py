4436e54b936a992571027e9b9eacebe28a922cf6
sopier <sopier@gmail.com>
2012-08-06 01:44:21 +0700

    script to count folder size recursively

---
+++ count_file_size.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import os
+
+folder = os.path.realpath(os.path.dirname(__file__))
+
+folder_size = 0
+
+for (path, dirs, files) in os.walk(folder):
+    for f in files:
+        filename = os.path.join(path, f)
+        try:
+            folder_size += os.path.getsize(filename)
+        except:
+            continue
+
+print "Ukuran Folder %s => %.1f MB" % (folder.split('/')[-1], folder_size / (1024*1024.0))
