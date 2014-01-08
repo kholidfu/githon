ad0939a141959b43d57b78d1cb3b8f3f22d8073d
sopier <sopier@gmail.com>
2014-01-08 11:43:40 +0700

    editing code

--- vectormetags.py
+++ vectormetags.py
@@ -1,15 +1,21 @@
 import urllib2
 from bs4 import BeautifulSoup
+import time
 
 url = "http://vector.me/tags/"
 opener = urllib2.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-html = opener.open(url + "1")
-soup = BeautifulSoup(html)
-
-tags = soup.find('div', attrs={'class': 'tags_cloud'})
-tags = tags.findAll('a')
 
 with open('/home/banteng/Desktop/vectormetags.txt', 'wa') as f:
-    for tag in tags:
-        f.write(tag.getText() + '\n')
+    for i in range(1, 101):
+        print "Starting %s" % url + str(i)
+        html = opener.open(url + str(i))
+        soup = BeautifulSoup(html)
+        tags = soup.find('div', attrs={'class': 'tags_cloud'})
+        tags = tags.findAll('a')
+
+        for tag in tags:
+            f.write(tag.getText() + '\n')
+
+        time.sleep(3)
+        print "wait for 3 second"
