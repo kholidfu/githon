e899a54fdb714607014a74e8cbe5d6040a5a6df7
sopier <sopier@gmail.com>
2014-01-10 10:08:33 +0700

    edit code

--- vectorfreaktags.py
+++ vectorfreaktags.py
@@ -6,7 +6,7 @@ url = "http://www.vectorfreak.com"
 opener = urllib2.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
-html = opener.open("http://www.vectorfreak.com/").read()
+html = opener.open(url).read()
 soup = BeautifulSoup(html)
 
 # begin parsing
@@ -16,11 +16,11 @@ ahrefs = div.findAll('a')
 
 freaktags = [i.getText() for i in ahrefs]
 
-with open('vectormetags.txt') as f:
+with open('/home/banteng/gitcode/githon/vectormetags.txt') as f:
     tags = f.readlines()
     tags = [tag.strip() for tag in tags]
 
-with open('vectormetags.txt', 'a') as f2:    
+with open('/home/banteng/gitcode/githon/vectormetags.txt', 'a') as f2:    
     for i in freaktags:
         if i not in tags:
             f2.write(i + '\n')
